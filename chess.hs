d7441340511b285f6ecc526a9b535407d2ddfcc6
Jaap Joris Vens <jj@rtts.eu>
2015-08-13 01:34:01 +0200

    messed up with class Read

--- chess.hs
+++ chess.hs
@@ -1,32 +1,34 @@
 data Piece = Piece Color Type deriving Eq
 data Color = White | Black deriving Eq
 data Type = King | Queen | Rook | Bishop | Knight | Pawn deriving Eq
+data Square = Square (Maybe Piece)
 data Board = Board [[Maybe Piece]] deriving Eq
 
-initialBoard = Board $ map (map readPiece) boardSpec
+initialBoard = Board $ map (map readPiece)
+               [['r','n','b','q','k','b','n','r'],
+                ['p','p','p','p','p','p','p','p'],
+                [' ',' ',' ',' ',' ',' ',' ',' '],
+                [' ',' ',' ',' ',' ',' ',' ',' '],
+                [' ',' ',' ',' ',' ',' ',' ',' '],
+                [' ',' ',' ',' ',' ',' ',' ',' '],
+                ['P','P','P','P','P','P','P','P'],
+                ['R','N','B','Q','K','B','N','R']]
 
-boardSpec = [['r','n','b','q','k','b','n','r'],
-             ['p','p','p','p','p','p','p','p'],
-             [' ',' ',' ',' ',' ',' ',' ',' '],
-             [' ',' ',' ',' ',' ',' ',' ',' '],
-             [' ',' ',' ',' ',' ',' ',' ',' '],
-             [' ',' ',' ',' ',' ',' ',' ',' '],
-             ['P','P','P','P','P','P','P','P'],
-             ['R','N','B','Q','K','B','N','R']]
-
-readPiece ' ' = Nothing
-readPiece 'r' = Just (Piece Black Rook)
-readPiece 'n' = Just (Piece Black Knight)
-readPiece 'b' = Just (Piece Black Bishop)
-readPiece 'q' = Just (Piece Black Queen)
-readPiece 'k' = Just (Piece Black King)
-readPiece 'p' = Just (Piece Black Pawn)
-readPiece 'R' = Just (Piece White Rook)
-readPiece 'N' = Just (Piece White Knight)
-readPiece 'B' = Just (Piece White Bishop)
-readPiece 'Q' = Just (Piece White Queen)
-readPiece 'K' = Just (Piece White King)
-readPiece 'P' = Just (Piece White Pawn)
+instance Read Square where
+--    readsPrec _ " " = [(Nothing, " ")]
+    readsPrec _ ('♔':s) = [(Square (Just (Piece White King)), s)]
+                  -- ("♕", Just (Piece White Queen)),
+                  -- ("♖", Just (Piece White Rook)),
+                  -- ("♗", Just (Piece White Bishop)),
+                  -- ("♘", Just (Piece White Knight)),
+                  -- ("♙", Just (Piece White Pawn)),
+                  -- ("♚", Just (Piece Black King)),
+                  -- ("♛", Just (Piece Black Queen)),
+                  -- ("♜", Just (Piece Black Rook)),
+                  -- ("♝", Just (Piece Black Bishop)),
+                  -- ("♞", Just (Piece Black Knight)),
+                  -- ("♟", Just (Piece Black Pawn))]
+    readsPrec _ _ = []
 
 instance Show Piece where
   show (Piece White King)   = "♔ "
@@ -42,23 +44,42 @@ instance Show Piece where
   show (Piece Black Knight) = "♞ "
   show (Piece Black Pawn)   = "♟ "
 
-data Square = Empty Color | Contains Piece Color deriving Eq
-instance Show Square where
-  show (Empty White) = "  "
-  show (Empty Black) = invertString "  "
-  show (Contains p White) = show p
-  show (Contains p Black) = invertString . show . invertPiece $ p
-invertPiece (Piece c t) = Piece (invertColor c) t
-invertString s = "\27[7m" ++ s ++ "\27[27m"
-invertColor Black = White
-invertColor White = Black
-
 instance Show Board where
-  show (Board lines) = foldr (++) "a b c d e f g h" lineStrings
-    where lineStrings = zipWith showLine lines [8,7..]
-          showLine squares n = foldr (++) (show n ++ "\n") squareStrings
-            where squareStrings = zipWith showSquare squares colors
-                  showSquare (Just p) color = show (Contains p color)
-                  showSquare Nothing color = show (Empty color)
-                  colors = if even n then concat $ repeat [White, Black]
-                           else concat $ repeat [Black, White]
+  show = showBoard
+
+showBoard :: Board -> String
+showBoard (Board lines) = foldr (++) "a b c d e f g h" lineStrings
+  where lineStrings = zipWith showLine lines [8,7..]
+
+showLine :: [Maybe Piece] -> Int -> String
+showLine squares n = foldr (++) (show n ++ "\n") squareStrings
+    where squareStrings = zipWith showSquare squares colors
+          colors = if even n then concat $ repeat [White, Black]
+                   else concat $ repeat [Black, White]
+
+showSquare :: Maybe Piece -> Color -> String
+showSquare Nothing White = "  "
+showSquare Nothing Black = invertString "  "
+showSquare (Just p) White = show p
+showSquare (Just p) Black = invertString . show . invertPiece $ p
+  where invertPiece (Piece c t) = Piece (invertColor c) t
+        invertColor Black = White
+        invertColor White = Black
+
+invertString :: String -> String
+invertString s = "\27[7m" ++ s ++ "\27[27m"
+
+readPiece :: Char -> Maybe Piece
+readPiece ' ' = Nothing
+readPiece 'r' = Just (Piece Black Rook)
+readPiece 'n' = Just (Piece Black Knight)
+readPiece 'b' = Just (Piece Black Bishop)
+readPiece 'q' = Just (Piece Black Queen)
+readPiece 'k' = Just (Piece Black King)
+readPiece 'p' = Just (Piece Black Pawn)
+readPiece 'R' = Just (Piece White Rook)
+readPiece 'N' = Just (Piece White Knight)
+readPiece 'B' = Just (Piece White Bishop)
+readPiece 'Q' = Just (Piece White Queen)
+readPiece 'K' = Just (Piece White King)
+readPiece 'P' = Just (Piece White Pawn)
