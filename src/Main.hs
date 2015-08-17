5d3a7a547d0d401c4f64e215e235d2d8c115dfdb
Jaap Joris Vens <jj@rtts.eu>
2015-08-17 21:56:08 +0200

    catch exceptions

--- src/Main.hs
+++ src/Main.hs
@@ -1,6 +1,8 @@
 module Main where
 
 import System.IO
+import System.Exit
+import Control.Exception
 import System.Console.Chess
 import Chess
 
@@ -10,12 +12,28 @@ main = do
   gameLoop defaultBoard
 
 gameLoop board = do
+  if (check White board)
+    then putStrLn "White is checked!"
+    else return ()
+  if (check Black board)
+    then putStrLn "Black is checked!"
+    else return ()
+  if (mate White board)
+    then do
+    putStrLn "Black has won. Congratulations!"
+    exitWith ExitSuccess
+    else return ()
+  if (mate Black board)
+    then do
+    putStrLn "White has won. Congratulations!"
+    exitWith ExitSuccess
+    else return ()
   putStr "Your move: "
   hFlush stdout
   move <- getLine
-  let newBoard = moveSAN move board
+  newBoard <- (evaluate $ moveSAN move board) `catch` (\(SomeException _) -> return $ Left NoParse)
   case newBoard of
-   Left _ -> do
+   Left e -> do
      putStrLn "Invalid move, try again..."
      gameLoop board
    Right b -> do
